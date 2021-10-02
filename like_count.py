70aae81609ced73f89110b1df46b0f94db7a8ff3
Martin Pichon <85355814+KentoNanami@users.noreply.github.com>
2021-10-02 15:18:20 +0200

    Update like_count.py

--- like_count.py
+++ like_count.py
@@ -2,5 +2,4 @@ from bs4 import BeautifulSoup as bs
 
 soup = bs(open('liked_posts.html', encoding="utf8"), 'html.parser')
 
-print(len(soup.find_all('a')))
-
+print("Number of posts liked  : ",len(soup.find_all('a')))
